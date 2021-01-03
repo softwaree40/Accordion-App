76672b6cab4aba8a74ca720a4aa93b691467b206
softwaree40 <softwaree40@gmail.com>
2021-01-03 16:18:48 -0500

    i am adding new logic

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,21 +1,28 @@
-import React from "react"
+import React, { useState } from  "react";
 
 
 
 const Accordion = ({items})=>{
+    const [activeIndex,setActiveIndex] = useState(null)
+    
+    const onTitleClick = (index)=>{
+       setActiveIndex(index)
+
+    }
    
-    const renderItems = items.map((item,id)=>{
+    const renderItems = items.map((item,index)=>{
 
       return (
-        <div key={item.id} className="container">
-           <div className="title active">
+        <div className="container">
+           <div onClick={()=> onTitleClick(index)} className="title active">
               <i  className="dropdown icon"></i>
                {item.title}
            </div>
            <div className="content active">
              <p>{item.content}</p>
+             
            </div>
-
+           {activeIndex}
       </div>
       )
 
