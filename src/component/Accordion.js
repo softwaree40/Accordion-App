1def5024bf284877b75e2044da3a2b8d18aa2d18
softwaree40 <softwaree40@gmail.com>
2021-01-03 16:48:57 -0500

    adding new style for active content

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -11,18 +11,19 @@ const Accordion = ({items})=>{
     }
    
     const renderItems = items.map((item,index)=>{
-
+    const active = activeIndex === index ? "active" : ""
+         
       return (
         <div className="container">
-           <div onClick={()=> onTitleClick(index)} className="title active">
+           <div onClick={()=> onTitleClick(index)} className={`title ${active}`}>
               <i  className="dropdown icon"></i>
                {item.title}
            </div>
-           <div className="content active">
+           <div className={`content ${active}`}>
              <p>{item.content}</p>
              
            </div>
-           {activeIndex}
+           
       </div>
       )
 
